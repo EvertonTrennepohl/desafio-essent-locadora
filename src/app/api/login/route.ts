70167dcde386e07eecf739ebb266432102574957
EvertonTrennepohl <gauchotp@gmail.com>
2023-06-28 20:58:49 -0300

    Added users

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function GET(){
     const users = await prisma.user.findMany();
     return new Response(JSON.stringify(users));
