97e670342b050d36a75228cdfa41c81117e40092
EvertonTrennepohl <gauchotp@gmail.com>
2023-06-28 21:32:46 -0300

    Removed users

--- src/app/users/page.tsx
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function Users(request: Request) {
-    const result = await fetch("https://desafio-essent-locadora-everton.vercel.app/login");
-    const data = result.json();
-    return NextResponse.json({data});
-}
